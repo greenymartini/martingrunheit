fc6db98c6c280470603c529ba2c2d681d3a1a635
Martin Grünheit <m.gruenheit@gmail.com>
2021-11-15 16:12:56 +0100

    first commit

---
+++ videos/turtlegab.py
@@ -0,0 +1,32 @@
+import turtle
+
+turtle.speed(100)
+
+
+def trianglecircle():
+    turtle.pencolor("black")
+    for i in range(100):
+        turtle.forward(200)
+        turtle.left(125)
+
+
+def letssee():
+
+    color = ["red", "black", "purple", "blue",
+             "yellow", "brown", "cyan", "white"]
+    pen = turtle.pencolor(color[0])
+    pen
+    for i in range(200):
+        turtle.up
+        turtle.goto(0, 0)
+        turtle.down
+        turtle.left(5)
+        turtle.circle(100)
+        print(pen)
+
+
+# trianglecircle()
+while True:
+    turtle.left(5)
+    letssee()
+    pen = "black"
