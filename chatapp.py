8bd4be217797e4da435a3efd89b1fa65bcf05ed6
Martin Grünheit <m.gruenheit@gmail.com>
2022-02-04 13:29:22 +0100

    twittterfeed

--- chatapp.py
+++ chatapp.py
@@ -16,9 +16,9 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
-    userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    userInput = request.args.get('msg')
+    return str(english_bot.get_response(userInput))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
